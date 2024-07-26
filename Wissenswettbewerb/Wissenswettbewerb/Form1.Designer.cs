51b5aaf223662ebd7dcac3a27af9d00899322b72
Yasin Kereci <kereciyasin52@gmail.com>
2024-07-26 23:15:26 +0200

    frage added

--- Wissenswettbewerb/Wissenswettbewerb/Form1.Designer.cs
+++ Wissenswettbewerb/Wissenswettbewerb/Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnA = new System.Windows.Forms.Button();
+            this.BtnB = new System.Windows.Forms.Button();
+            this.BtnC = new System.Windows.Forms.Button();
+            this.BtnD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFrage = new System.Windows.Forms.Label();
+            this.lblTrue = new System.Windows.Forms.Label();
+            this.lblFalsch = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(778, 319);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnA
+            // 
+            this.BtnA.Location = new System.Drawing.Point(15, 358);
+            this.BtnA.Name = "BtnA";
+            this.BtnA.Size = new System.Drawing.Size(344, 97);
+            this.BtnA.TabIndex = 2;
+            this.BtnA.Text = "A";
+            this.BtnA.UseVisualStyleBackColor = true;
+            // 
+            // BtnB
+            // 
+            this.BtnB.Location = new System.Drawing.Point(449, 358);
+            this.BtnB.Name = "BtnB";
+            this.BtnB.Size = new System.Drawing.Size(344, 97);
+            this.BtnB.TabIndex = 3;
+            this.BtnB.Text = "B";
+            this.BtnB.UseVisualStyleBackColor = true;
+            // 
+            // BtnC
+            // 
+            this.BtnC.Location = new System.Drawing.Point(15, 461);
+            this.BtnC.Name = "BtnC";
+            this.BtnC.Size = new System.Drawing.Size(344, 97);
+            this.BtnC.TabIndex = 4;
+            this.BtnC.Text = "C";
+            this.BtnC.UseVisualStyleBackColor = true;
+            // 
+            // BtnD
+            // 
+            this.BtnD.Location = new System.Drawing.Point(449, 461);
+            this.BtnD.Name = "BtnD";
+            this.BtnD.Size = new System.Drawing.Size(344, 97);
+            this.BtnD.TabIndex = 5;
+            this.BtnD.Text = "D";
+            this.BtnD.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(803, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 35);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Soru No : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(820, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 35);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dogru :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(829, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 35);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Yanlis : ";
+            // 
+            // lblFrage
+            // 
+            this.lblFrage.AutoSize = true;
+            this.lblFrage.Location = new System.Drawing.Point(938, 43);
+            this.lblFrage.Name = "lblFrage";
+            this.lblFrage.Size = new System.Drawing.Size(29, 35);
+            this.lblFrage.TabIndex = 9;
+            this.lblFrage.Text = "0";
+            // 
+            // lblTrue
+            // 
+            this.lblTrue.AutoSize = true;
+            this.lblTrue.Location = new System.Drawing.Point(938, 91);
+            this.lblTrue.Name = "lblTrue";
+            this.lblTrue.Size = new System.Drawing.Size(29, 35);
+            this.lblTrue.TabIndex = 10;
+            this.lblTrue.Text = "0";
+            // 
+            // lblFalsch
+            // 
+            this.lblFalsch.AutoSize = true;
+            this.lblFalsch.Location = new System.Drawing.Point(938, 141);
+            this.lblFalsch.Name = "lblFalsch";
+            this.lblFalsch.Size = new System.Drawing.Size(29, 35);
+            this.lblFalsch.TabIndex = 11;
+            this.lblFalsch.Text = "0";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(809, 210);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(158, 79);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = "Sonraki";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(824, 369);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1029, 369);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(199, 188);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(505, 611);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 35);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(638, 611);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 35);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 35F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1234, 726);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblFalsch);
+            this.Controls.Add(this.lblTrue);
+            this.Controls.Add(this.lblFrage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnD);
+            this.Controls.Add(this.BtnC);
+            this.Controls.Add(this.BtnB);
+            this.Controls.Add(this.BtnA);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Corbel", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnA;
+        private System.Windows.Forms.Button BtnB;
+        private System.Windows.Forms.Button BtnC;
+        private System.Windows.Forms.Button BtnD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblFrage;
+        private System.Windows.Forms.Label lblTrue;
+        private System.Windows.Forms.Label lblFalsch;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Wissenswettbewerb/Wissenswettbewerb/Form1.cs
+++ Wissenswettbewerb/Wissenswettbewerb/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace Wissenswettbewerb
 {
+
     public partial class Form2 : Form
     {
         public Form2()
         {
             InitializeComponent();
         }
+
+        int frageNo = 0, frageTrue = 0, frageFalsch = 0;
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            frageNo++;
+            lblFrage.Text = frageNo.ToString();
+
+            if (frageNo == 1)
+            {
+                richTextBox1.Text = "Was ist die Hauptstadt der Schweiz?";
+                BtnA.Text = "Bern";
+                BtnB.Text = "Zürich";
+                BtnC.Text = "Basel";
+                BtnD.Text = "Paris";
+                label4.Text = "Bern";
+
+            }
+            if(frageNo == 2)
+            {
+                richTextBox1.Text = "Welche Sprache gehört nicht zu den offiziellen Sprachen der Schweiz?";
+                BtnA.Text = "Englisch";
+                BtnB.Text = "Deutsch";
+                BtnC.Text = "Türkisch";
+                BtnD.Text = "Französisch";
+                label4.Text = "Deutsch";
+            }
+            if (frageNo == 3)
+            {
+                richTextBox1.Text = "Welcher ist der höchste Berg der Schweiz?";
+                BtnA.Text = "Pilatus";
+                BtnB.Text = "Matterhorn";
+                BtnC.Text = "Jungfrau";
+                BtnD.Text = "Dufourspitze";
+                label4.Text = "Jungfrau";
+            }
+            }
     }
 }
