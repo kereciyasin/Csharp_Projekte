f12701d3009ead10f717adb0bb6e6347b0cf27a3
Yasin Kereci <kereciyasin52@gmail.com>
2024-08-06 19:24:49 +0200

    sql baglama

---
+++ Projekt_Spital/Projekt_Spital/sql.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Projekt_Spital
+{
+    internal class sql
+    {
+
+        public SqlConnection Connection()
+        {
+            SqlConnection verbinden = new SqlConnection("Data Source=kereci\\SQLEXPRESS;Initial Catalog=Spital_Projekt;Integrated Security=True;Trust Server Certificate=True");
+        
+        verbinden.Open();
+        return verbinden;   
+        }
+
+
+    }
+}
